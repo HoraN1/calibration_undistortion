86244642a1d54be3b4405a37a11956623b0268b4
HoraN1 <horacesunhe@gmail.com>
2019-08-05 17:13:13 +0800

    calibration

---
+++ calibration/1.py
@@ -0,0 +1,82 @@
+"""
+    Project: Calibration-intrinsic
+        to get the calibrated images
+    Author: He Zhanxin
+    Time:4/8/2017
+"""
+
+import numpy as np
+import cv2
+import glob
+import os
+
+
+def calibration(sample_img):
+    # termination criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+    # Check board size
+    cbrow = 6
+    cbcol = 9
+
+    # prepare object points
+    objp = np.zeros((cbrow * cbcol, 3), np.float32)
+    objp[:, :2] = np.mgrid[0:cbcol, 0:cbrow].T.reshape(-1, 2)
+
+    # Arrays to store object points and image points from all the images.
+    obj_points = []  # 3d point in real world space
+    img_points = []  # 2d points in image plane.
+
+    # import image
+    img = sample_img
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    h, w = img.shape[:2]
+
+    # Find the chess board corners
+    ret, corners = cv2.findChessboardCorners(gray_img, (cbcol, cbrow), None)
+
+    # If found, add object points, image points (after refining them)
+    if ret:
+        obj_points.append(objp)
+
+        corners2 = cv2.cornerSubPix(gray_img, corners, (11, 11), (-1, -1), criteria)
+        img_points.append(corners2)
+
+        # Draw and display the corners
+        img = cv2.drawChessboardCorners(gray_img, (cbcol, cbrow), corners2, ret)
+
+    ret, mtx, dist_coe, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, gray_img.shape[::-1], None, None)
+    new_camera_mtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist_coe, (w, h), 1, (w, h))
+
+    # un-distort
+    dst = cv2.undistort(img, mtx, dist_coe, new_camera_mtx)
+
+    # plt.imshow(dst)
+    # plt.show()
+    # crop the image
+    x, y, w, h = roi
+    return dst[y:y + h, x:x + w]
+
+
+# The paths where the sample images are and where the calibrated images go
+sample_path = '/home/hora/PycharmProjects/calibration/pictures/'
+cali_path = '/home/hora/PycharmProjects/calibration/calibrated/'
+
+# Import images
+
+dist_images = glob.glob(os.path.join(sample_path, '*.jpg'))
+img_index = 1
+
+for sample in dist_images:
+    img_name = cv2.imread(sample)
+
+    undistorted_img = calibration(img_name)
+    cv2.imwrite(os.path.join(cali_path, 'calibrated-sample{}.jpg'.format(img_index)), undistorted_img)
+
+    calibrated_img = cv2.imread(os.path.join(cali_path, 'calibrated-sample{}.jpg').format(img_index))
+    cv2.imshow('img{}'.format(img_index), calibrated_img)
+    img_index += 1
+
+    k = cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
---
+++ calibration/test.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import pathlib2 as pathlib
+# The direction os images
+img_dir = pathlib.Path('/home/hora/PycharmProjects/calibration/pictures/')
+print(img_dir)
+# Import images
+# images = glob.glob('/home/hora/PycharmProjects/calibration/pictures/*.jpg')
+
+
+# plt.imshow(gray_img)
+# plt.show()
